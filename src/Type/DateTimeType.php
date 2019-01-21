18ddd15e7fd008e027ea863fc5c555fbd7d30cca
Simon Podlipsky <simon@podlipsky.net>
2019-01-21 10:23:15 +0100

    Support ISO 8601 for DateTime type

--- src/Type/DateTimeType.php
+++ src/Type/DateTimeType.php
@@ -12,12 +12,17 @@ use GraphQL\Language\AST\ValueNode;
 use GraphQL\Type\Definition\CustomScalarType;
 use GraphQL\Utils\Utils;
 use InvalidArgumentException;
-use function sprintf;
+use function assert;
+use function preg_match;
+use function strpos;
+use function substr;
 
 class DateTimeType extends CustomScalarType
 {
-    private const NAME        = 'DateTime';
-    private const DESCRIPTION = 'The `DateTime` scalar type represents time data, represented as an ISO-8601 encoded UTC date string.';
+    private const NAME           = 'DateTime';
+    private const DESCRIPTION    = 'The `DateTime` scalar type represents time data, represented as an ISO-8601 encoded UTC date string.';
+    private const RFC_3339_REGEX = '~^(\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][\d]|3[01])T([01][\d]|2[0-3]):([0-5][\d]):([0-5][\d]|60))(\.\d{1,})?(([Z])|([+|-]([01][\d]|2[0-3]):[0-5][\d]))$~';
+//    private const RFC_3339_REGEX_DATE = '~^(\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][\d]|3[01]))$~';
 
     /** @var string */
     public $name = self::NAME;
@@ -54,18 +59,11 @@ class DateTimeType extends CustomScalarType
      */
     public function parseValue($value) : DateTimeImmutable
     {
-        $datetime = DateTimeImmutable::createFromFormat(DateTimeInterface::ATOM, $value);
-
-        if ($datetime === false) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'DateTime type expects input value to be of ATOM format (%s).',
-                    (new DateTimeImmutable())->format(DateTimeInterface::ATOM)
-                )
-            );
+        if (! $this->validateDatetime($value)) {
+            throw new InvalidArgumentException('DateTime type expects input value to be ISO 8601 compliant.');
         }
 
-        return $datetime;
+        return new DateTimeImmutable($value);
     }
 
     /**
@@ -81,5 +79,62 @@ class DateTimeType extends CustomScalarType
         }
 
         return $this->parseValue($valueNode->value);
+    }
+
+    private function validateDatetime(string $value) : bool
+    {
+        if (preg_match(self::RFC_3339_REGEX, $value) !== 1) {
+            return false;
+        }
+
+        $tPosition = strpos($value, 'T');
+        assert($tPosition !== false);
+        if (! $this->validateDate(substr($value, 0, $tPosition))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function validateDate(string $date) : bool
+    {
+//        if (preg_match(self::RFC_3339_REGEX_DATE, $date) !== 1) {
+//            return false;
+//        }
+
+        // Verify the correct number of days for
+        // the month contained in the date-string.
+        $year  = (int) substr($date, 0, 4);
+        $month = (int) substr($date, 5, 2);
+        $day   = (int) substr($date, 8, 2);
+
+        switch ($month) {
+            case 2: // February
+                if ($this->isLeapYear($year) && $day > 29) {
+                    return false;
+                }
+
+                if (! $this->isLeapYear($year) && $day > 28) {
+                    return false;
+                }
+
+                return true;
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                if ($day > 30) {
+                    return false;
+                }
+
+                break;
+        }
+
+        return true;
+    }
+
+    private function isLeapYear(int $year) : bool
+    {
+        return ($year % 4 === 0 && $year % 100 !== 0) || $year % 400 === 0;
     }
 }
--- tests/Type/DateTimeTypeTest.php
+++ tests/Type/DateTimeTypeTest.php
@@ -43,26 +43,63 @@ final class DateTimeTypeTest extends TestCase
     public function testSerializeInvalidType() : void
     {
         $this->expectException(InvariantViolation::class);
+        $this->expectExceptionMessage('DateTime is not an instance of DateTimeImmutable nor DateTime:');
 
         $dateTimeType = new DateTimeType();
         $dateTimeType->serialize('non datetimetype');
     }
 
-    public function testParseValue() : void
+    /**
+     * @dataProvider dataProviderParseValue
+     */
+    public function testParseValue(string $valueToParse, DateTimeImmutable $expected) : void
     {
         $dateTimeType = new DateTimeType();
 
-        self::assertEquals(
-            new DateTimeImmutable('2018-12-31 01:02:03'),
-            $dateTimeType->parseValue('2018-12-31T01:02:03+00:00')
-        );
+        self::assertEquals($expected, $dateTimeType->parseValue($valueToParse));
     }
-    public function testParseValueInvalidFormat() : void
+
+    /**
+     * @return mixed[][]
+     */
+    public function dataProviderParseValue() : iterable
+    {
+        // Datetime with hours, minutes and seconds
+        yield ['2016-02-01T00:00:15Z', new DateTimeImmutable('2016-02-01 0:0:15')];
+        yield ['2016-11-01T00:00:00-11:00', new DateTimeImmutable('2016-11-01 11:00:00')];
+        yield ['2017-01-07T11:25:00+01:00', new DateTimeImmutable('2017-01-07 10:25')];
+        yield ['2017-01-07T00:00:00+01:20', new DateTimeImmutable('2017-01-06 22:40')];
+        // Datetime with hours, minutes, seconds and fractional seconds
+        yield ['2016-02-01T00:00:00.1Z', new DateTimeImmutable('2016-02-01 00:00:00.100')];
+        yield ['2016-02-01T00:00:00.000Z', new DateTimeImmutable('2016-02-01')];
+        yield ['2016-02-01T00:00:00.990Z', new DateTimeImmutable('2016-02-01 00:00:00.990')];
+        yield ['2016-02-01T00:00:00.23498Z', new DateTimeImmutable('2016-02-01 00:00:00.23498')];
+        yield ['2017-01-07T11:25:00.450+01:00', new DateTimeImmutable('2017-01-07 10:25:0.450')];
+    }
+
+    /**
+     * @dataProvider dataProviderParseValueInvalidFormatOrValue
+     */
+    public function testParseValueInvalidFormatOrValue(string $value) : void
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('DateTime type expects input value to be ISO 8601 compliant.');
 
         $dateTimeType = new DateTimeType();
-        $dateTimeType->parseValue('not-datetime string');
+        $dateTimeType->parseValue($value);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function dataProviderParseValueInvalidFormatOrValue() : iterable
+    {
+        yield ['2017-01-001T00:00:00Z'];
+        yield ['2017-02-31T00:00:00Z'];
+        yield ['not-datetime string'];
+        yield ['2018-11-31T00:00:00Z'];
+        yield ['2019-02-29T00:00:00Z'];
+        yield ['2020-02-30T00:00:00Z'];
     }
 
     public function testParseLiteral() : void
