4e364ef3518bccca1428b419077eff856bd3a31b
Simon Podlipsky <simon@podlipsky.net>
2021-09-27 18:45:04 +0200

    Improve DateTime type tests (#103)

--- tests/Type/DateTimeTypeTest.php
+++ tests/Type/DateTimeTypeTest.php
@@ -30,9 +30,9 @@ final class DateTimeTypeTest extends TestCase
     }
 
     /**
-     * @return mixed[][]|Generator
+     * @return Generator<int, array{string, DateTimeInterface}>
      */
-    public function dateProviderSerialize(): iterable
+    public function dateProviderSerialize(): Generator
     {
         yield ['2018-12-31T01:02:03+00:00', new DateTime('2018-12-31 01:02:03')];
         yield ['2018-12-31T01:02:03+00:00', new DateTimeImmutable('2018-12-31 01:02:03')];
@@ -44,7 +44,7 @@ final class DateTimeTypeTest extends TestCase
     public function testSerializeInvalidType(): void
     {
         $this->expectException(InvariantViolation::class);
-        $this->expectExceptionMessage('DateTime is not an instance of DateTimeImmutable nor DateTime:');
+        $this->expectExceptionMessage('DateTime is not an instance of DateTimeImmutable nor DateTime: non datetimetype');
 
         $dateTimeType = new DateTimeType();
         $dateTimeType->serialize('non datetimetype');
@@ -64,22 +64,33 @@ final class DateTimeTypeTest extends TestCase
     }
 
     /**
-     * @return mixed[][]
+     * @return Generator<string, array{string, DateTimeImmutable}>
      */
-    public function dataProviderParseValue(): iterable
+    public function dataProviderParseValue(): Generator
     {
         // Datetime with hours, minutes and seconds
-        yield ['2016-02-01T00:00:15Z', new DateTimeImmutable('2016-02-01 0:0:15')];
-        yield ['2016-11-01T00:00:00-11:00', new DateTimeImmutable('2016-11-01 11:00:00')];
-        yield ['2017-01-07T11:25:00+01:00', new DateTimeImmutable('2017-01-07 10:25')];
-        yield ['2017-01-07T00:00:00+01:20', new DateTimeImmutable('2017-01-06 22:40')];
+        yield 'timezone #1' => ['2016-11-01T00:00:00-11:00', new DateTimeImmutable('2016-11-01 11:00:00')];
+        yield 'timezone #2' => ['2017-01-07T00:00:00+01:20', new DateTimeImmutable('2017-01-06 22:40')];
+        yield 'January last day' => ['2000-01-31T00:00:00Z', new DateTimeImmutable('2000-01-31 00:00')];
+        yield 'February last day' => ['2001-02-28T00:00:00Z', new DateTimeImmutable('2001-02-28 00:00')];
+        yield 'February leap year last day' => ['2000-02-29T00:00:00Z', new DateTimeImmutable('2000-02-29 00:00')];
+        yield 'March last day' => ['2000-03-31T00:00:00Z', new DateTimeImmutable('2000-03-31 00:00')];
+        yield 'April last day' => ['2000-04-30T00:00:00Z', new DateTimeImmutable('2000-04-30 00:00')];
+        yield 'May last day' => ['2000-05-31T00:00:00Z', new DateTimeImmutable('2000-05-31 00:00')];
+        yield 'June last day' => ['2000-06-30T00:00:00Z', new DateTimeImmutable('2000-06-30 00:00')];
+        yield 'July last day' => ['2000-07-31T00:00:00Z', new DateTimeImmutable('2000-07-31 00:00')];
+        yield 'August last day' => ['2000-08-31T00:00:00Z', new DateTimeImmutable('2000-08-31 00:00')];
+        yield 'September last day' => ['2000-09-30T00:00:00Z', new DateTimeImmutable('2000-09-30 00:00')];
+        yield 'Octover last day' => ['2000-10-31T00:00:00Z', new DateTimeImmutable('2000-10-31 00:00')];
+        yield 'November last day' => ['2000-11-30T00:00:00Z', new DateTimeImmutable('2000-11-30 00:00')];
+        yield 'December last day' => ['2000-12-31T00:00:00Z', new DateTimeImmutable('2000-12-31 00:00')];
 
         // Datetime with hours, minutes, seconds and fractional seconds
-        yield ['2016-02-01T00:00:00.1Z', new DateTimeImmutable('2016-02-01 00:00:00.100')];
-        yield ['2016-02-01T00:00:00.000Z', new DateTimeImmutable('2016-02-01')];
-        yield ['2016-02-01T00:00:00.990Z', new DateTimeImmutable('2016-02-01 00:00:00.990')];
-        yield ['2016-02-01T00:00:00.23498Z', new DateTimeImmutable('2016-02-01 00:00:00.23498')];
-        yield ['2017-01-07T11:25:00.450+01:00', new DateTimeImmutable('2017-01-07 10:25:0.450')];
+        yield '1 fraction' => ['2016-02-01T00:00:00.1Z', new DateTimeImmutable('2016-02-01 00:00:00.100')];
+        yield '3 0fractions' => ['2016-02-01T00:00:00.000Z', new DateTimeImmutable('2016-02-01')];
+        yield '3 fractions' => ['2016-02-01T00:00:00.990Z', new DateTimeImmutable('2016-02-01 00:00:00.990')];
+        yield '6 fractions' => ['2016-02-01T00:00:00.23498Z', new DateTimeImmutable('2016-02-01 00:00:00.23498')];
+        yield 'fractions with timezone' => ['2017-01-07T11:25:00.450+01:00', new DateTimeImmutable('2017-01-07 10:25:0.450')];
     }
 
     /**
@@ -95,16 +106,18 @@ final class DateTimeTypeTest extends TestCase
     }
 
     /**
-     * @return string[][]
+     * @return Generator<int, array{string}>
      */
-    public function dataProviderParseValueInvalidFormatOrValue(): iterable
+    public function dataProviderParseValueInvalidFormatOrValue(): Generator
     {
+        yield ['2021-02-29T00:00:00Z'];
+        yield ['1900-02-29T00:00:00Z'];
         yield ['2017-01-001T00:00:00Z'];
         yield ['2017-02-31T00:00:00Z'];
-        yield ['not-datetime string'];
         yield ['2018-11-31T00:00:00Z'];
         yield ['2019-02-29T00:00:00Z'];
         yield ['2020-02-30T00:00:00Z'];
+        yield ['not-datetime string'];
     }
 
     public function testParseLiteral(): void
